1e0701ec38edc0cc7a28f5cef369433cabd62b71
Karim de Fombelle <karimdefombelle@azel-et-karim.local>
2023-06-16 22:48:39 +0800

    Java 17

---
+++ xmlcompare-core/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module fr.kdefombelle.xmlcompare.core {
+    requires slf4j.api;
+    requires org.xmlunit;
+    requires java.xml;
+    requires java.desktop;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+    exports fr.kdefombelle.xmlcompare.core;
+}
---
+++ xmlcompare-gui/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module fr.kdefombelle.xmlcompare.gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires slf4j.api;
+    requires java.desktop;
+    requires fr.kdefombelle.xmlcompare.core;
+}
