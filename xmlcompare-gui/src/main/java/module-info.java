efef50d90abcd9bb166b6cce34e62e7805831e48
Karim de Fombelle <karim.defombelle@gmail.com>
2025-11-22 15:53:36 +0800

    upgrade to javafx 25.0.1

--- xmlcompare-gui/src/main/java/module-info.java
+++ xmlcompare-gui/src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module fr.kdefombelle.xmlcompare.gui {
     requires fr.kdefombelle.xmlcompare.core;
+    opens fr.kdefombelle.xmlcompare.gui to javafx.fxml;
     requires org.slf4j;
     requires org.xmlunit;
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    exports fr.kdefombelle.xmlcompare.gui;
 }
